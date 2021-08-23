4215b3b4b6d3a59a1602b1694f74ac7b5070926e
pulidongz <paogravoso@gmail.com>
2021-08-23 22:21:31 +0800

    Added run http/https for Express

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import fs from 'fs';
 import database from './src/utils/database.js';
 
 import UserManagementRouter from './src/components/UserManagement/routes/UserManagementRouter.js';
@@ -28,24 +29,6 @@ app.use(express.urlencoded({ extended: true }));
 //Bind connection to error event (to get notification of connection errors)
 database.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
-
-app.get('/:users',(req, res)=>{
-    const users = req.params.users;
-
-    Users.findOne({username: users})
-    .then(data => {
-      if(data){
-        res.status(200).send(data);
-      } else {
-        res.status(404).send("User not found");
-      }
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
-});
-
 app.use('/api/user_management', UserManagementRouter);
 app.use('/api/data_visualization', (req, res) => res.send('Data visualization api'));
 app.use('/api/community_risk_assessment', (req, res) => res.send('Community risk assessment api'));
@@ -55,3 +38,20 @@ app.use('/api/subsurface_data', (req, res) => res.send('Subsurface data api'));
 app.use('/api/earthquake_data', (req, res) => res.send('Earthquake data api'));
 app.use('/api/rainfall_data', (req, res) => res.send('Rainfall data api'));
 app.use('/api/alert_generation', (req, res) => res.send('Alert generation api'));
+
+// ***uncomment when running in production***
+// Listen to both http & https ports
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer({
+//   key: fs.readFileSync('/location/of/privkey.pem'),
+//   cert: fs.readFileSync('location/of/cert.pem'),
+// }, app);
+// httpServer.listen(80, () => {
+//   console.log('HTTP Server running on port 80');
+// });
+
+// httpsServer.listen(443, () => {
+//   console.log('HTTPS Server running on port 443');
+// });
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
