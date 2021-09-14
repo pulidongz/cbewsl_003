5393b34a87d4ade621cd950702f3bb37e413e79c
pulidongz <paogravoso@gmail.com>
2021-09-15 07:42:07 +0800

    DB notes

--- packages/server/src/utils/database.js
+++ packages/server/src/utils/database.js
@@ -10,22 +10,25 @@ let options = {
   useUnifiedTopology: true,
   useCreateIndex: true
 }
+
 const commons_url = 'COMMONS_DB: mongodb://'+ process.env.DB_HOST +':27017/cbewsl_commons_db'
 const cbewsl_site_collections_url = 'CBEWSL_DB: mongodb://'+ process.env.DB_HOST +':27017/cbewsl_site_collections_db'
 const senslope_url = 'SENSLOPE_DB: mongodb://'+ process.env.DB_HOST +':27017/senslopedb'
 
+// DB for Sessions, User Management, and other data
 connDb.Commons = mongoose.createConnection(commons_url, options)
 .on("error", console.error.bind(console, "MongoDB Connection Error>> : "))
 .once("open", function() {
   console.log(commons_url);
 });
-
+// DB for CRA, Maintenance Logs, Incident Report Logs, and other data
 connDb.SiteCollections = mongoose.createConnection(cbewsl_site_collections_url, options)
 .on("error", console.error.bind(console, "MongoDB Connection Error>> : "))
 .once("open", function() {
   console.log(cbewsl_site_collections_url);
 });
 
+// Main senslope DB
 connDb.Senslope = mongoose.createConnection(senslope_url, options)
 .on("error", console.error.bind(console, "MongoDB Connection Error>> : "))
 .once("open", function() {
